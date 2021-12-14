cbe28644a73c6a0b1f8ed043bd5b0cb4fdfbbb69
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2021-12-14 22:40:53 +0300

    Add files via upload

--- MyShell/cikar.c
+++ MyShell/cikar.c
@@ -1,38 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int strtoint( char* charnums)
-{
- int number=0;
- int index=0;
- while(charnums[index])
- {
-    if(('0'<=charnums[index]) && (charnums[index]<='9'))
-    {
 
-    if(!number)
-        number= ( (int) charnums[index]) - 48;
-    else
-    {
-        number = (number *= 10) + ((int) charnums[index] - 48);
-    }
-    index++;
-         }
-         else
-         {
-            number=-1;
-            printf("\nGecersiz islem");
-            break;
-         }
- }
- return number;
-}
 
 int main(int argc, char *argv[], char**envp2)
 {  
    // 'islem cikar x y'   için sayilari 'x-y' şeklinde çıkartıp döndürüyor döndürüyor.
-   int x=strtoint(argv[2]);
-   int y=strtoint(argv[3]);
+   int x=atoi(argv[2]);
+   int y=atoi(argv[3]);
    int sonuc=x-y;
    return sonuc;
 }
--- MyShell/tekrar.c
+++ MyShell/tekrar.c
@@ -1,40 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int strtoint( char* charnums)
-{
- int number=0;
- int index=0;
- while(charnums[index])
- {
-    if(('0'<=charnums[index]) && (charnums[index]<='9'))
-    {
 
-    if(!number)
-        number= ( (int) charnums[index]) - 48;
-    else
-    {
-        number = (number *= 10) + ((int) charnums[index] - 48);
-    }
-    index++;
-         }
-         else
-         {
-            number=-1;
-            printf("\nGecersiz islem");
-            break;
-         }
- }
- return number;
-}
 
 int main(int argc, char *argv[])
 {  
-   int count =strtoint(argv[2]);
+   int count =atoi(argv[2]);
    for(int i=0;i<count;i++){
    printf("%s\n",argv[1]);
    } 
-
-
    return 0;
 }
